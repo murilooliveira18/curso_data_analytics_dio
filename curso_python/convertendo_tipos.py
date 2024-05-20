9dd637636a4734a5092e4b81fae4fdf019885a7b
Murilo Oliveira <84341631+murilooliveira18@users.noreply.github.com>
2024-05-20 19:40:09 -0300

    Primeiro commit
    commit com alguns arquivos feitos em aula no primeiro módulo de python do bootcamp da DIO

---
+++ curso_python/convertendo_tipos.py
@@ -0,0 +1,13 @@
+print(int(1.97348728)) #Convertendo float pra int
+print(int("10")) #Convertendo string pra int
+print(float("10.10")) #Convertendo string para float
+print(float(100)) #Convertendo int para float
+
+#Convertendo int para string e exibindo o tipo no terminal
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2) #Exibe como float no terminal
+print(100 // 2) #Exibe como int no terminal
---
+++ curso_python/variaveis_e_constantes.py
@@ -0,0 +1,12 @@
+nome = "Murilo"
+idade = 20
+
+nome, idade = "Mariana", 21
+
+print (nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
