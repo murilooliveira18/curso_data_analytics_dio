949a4c4f0709b29ffad368be4c52d5f61a4852bb
Murilo Oliveira <84341631+murilooliveira18@users.noreply.github.com>
2024-05-22 20:03:12 -0300

    inserindo operadores de comparação
    inserindo operadores de comparação que aprendi na aula

---
+++ curso_python/operadores_comparacao.py
@@ -0,0 +1,9 @@
+saldo = 200
+saque = 200
+
+print(saldo == saque) #igualdade
+print(saldo != saque) #diferença
+print(saldo > saque) #maior que
+print(saldo >= saque) #maior ou igual
+print(saldo < saque)  #menor que
+print(saldo <= saque) #menor ou igual
