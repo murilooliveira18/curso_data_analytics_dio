96721e884606fdde44e63dd28f74a428c7e350b9
Murilo Oliveira <84341631+murilooliveira18@users.noreply.github.com>
2024-05-23 21:06:53 -0300

    Manipulando strings com Python
    apredendo a manipular string com python

---
+++ curso_python/string_2.py
@@ -0,0 +1,23 @@
+nome = "Murilo"
+idade = 20
+profissao = "Progamador"
+linguagem = "Python"
+saldo = 45.435
+
+dados = {"nome": "Murilo", "idade": "20"}
+
+print("Nome: %s Idade: %d" % (nome, idade))
+
+print("Nome: {} Idade: {}" .format(nome, idade))
+
+print("Nome: {1} Idade: {0}" .format(idade, nome))
+
+print("Nome: {nome} Idade: {idade}" .format(nome=nome, idade=idade))
+
+print("Nome: {name} Idade: {age}" .format(age=idade, name=nome))
+
+print("Nome: {nome} Idade: {idade}" .format(**dados))
+
+print(f"Nome: {nome} Idade: {idade}")
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:.2f}")
+print(f"Nome: {nome} Idade: {idade} Saldo: {saldo:10.2f}")
---
+++ curso_python/string_3.py
@@ -0,0 +1,10 @@
+nome = "Murilo Oliveira Martins Delmondes"
+
+print(nome[0])
+print(nome[-2])
+print(nome[:6])
+print(nome[7:])
+print(nome[7:16])
+print(nome[7:16:2])
+print(nome[:])
+print(nome[::-1])
